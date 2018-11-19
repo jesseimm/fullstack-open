d94beb23e074416b3ca5262e7da0f64c63e971e2
Jesse Immonen <jesse.immonen@gmail.com>
2018-11-19 05:37:53 +0200

    Add proxy and add space between name and number

--- vk2/puhelinluottelo/src/App.jsx
+++ vk2/puhelinluottelo/src/App.jsx
@@ -11,8 +11,7 @@ const Notification = ({ message }) => {
 const Tietue = ({ person, removePerson }) => {
   return (
     <p>
-      {person.name}
-      {person.number}
+      {person.name} {person.number}
       <button onClick={() => removePerson(person.id)}>remove</button>
     </p>
   );
