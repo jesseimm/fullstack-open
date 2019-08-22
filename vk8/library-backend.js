363cec773b374b9f1d33cd28ebaac3e7b81ae72d
Jesse Immonen <jesse.immonen@gmail.com>
2019-08-22 18:25:21 +0300

    8.4

--- vk8/library-backend.js
+++ vk8/library-backend.js
@@ -104,7 +104,7 @@ const typeDefs = gql`
         hello: String!
         bookCount: Int!
         authorCount: Int! 
-        allBooks: [Book]!
+        allBooks(author: String): [Book]!
         allAuthors: [Author]!
     }
 `;
@@ -114,7 +114,11 @@ const resolvers = {
         hello: () => { return "world" },
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (root, args) => {
+            return args.author ?
+                books.filter((book) => book.author === args.author) :
+                books;
+        },
         allAuthors: () => authors
     },
 
